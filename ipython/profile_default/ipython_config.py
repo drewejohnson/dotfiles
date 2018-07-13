bcd36af79ec02a2db097af5e0b1bcd3235df529d
Andrew Johnson <drewej@protonmail.com>
2018-07-13 15:04:34 -0400

    IPY Auto-import pyplot in config

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -6,7 +6,6 @@ c = get_config()
 c.InteractiveShellApp.exec_lines = [
     'from importlib import reload',
     'import numpy',
-    'from matplotlib import pyplot',
 ]
 
 c.InteractiveShell.editor = 'vim'
