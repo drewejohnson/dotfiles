53263167c0170620b9c61aef7e4c93f3f0151b57
Andrew Johnson <drewej@protonmail.com>
2018-07-14 18:31:28 -0400

    ST Patched source code pro fonts

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Source Code Pro for Powerline:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
