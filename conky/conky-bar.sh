5933e05273084680291141638986518d581f67e2
Andrew Johnson <drewej@protonmail.com>
2019-03-18 19:06:02 -0400

    CONKY Initial conky-based statusline -> JSON
    Can use the conky-bar script inside i3bar

---
+++ conky/conky-bar.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Shell script to send JSON information status information from
+# conky to i3bar
+
+echo '{"version":1}'
+echo '['
+echo '[],'
+
+exec conky -c ~/.config/conky/conky-stat.conf
